78c1bb103b59eda114139960283d96d484a4e117
Lingasri <lingasri5@gmail.com>
2024-03-18 11:45:14 +0530

    -Initialization is done to the file structure.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+import WeatherAppComponent from "./Components/WeatherAppComponent/WeatherAppComponent";
+import QueryWeatherAppComponent from "./Components/QueryWeatherAppComponent/QueryWeatherAppComponent";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient=new QueryClient()
+
+function App() {
+  return (
+    <React.Fragment>
+      <QueryClientProvider client={queryClient}>
+        <QueryWeatherAppComponent/>
+        <WeatherAppComponent/>
+      </QueryClientProvider>
+    </React.Fragment>
+ 
+  )
+  
+}
+
+ export default App;
